b679503e28300d5d46520aa65ca3657a64ae1b13
Jacekhacking <82614083+Jacekhacking@users.noreply.github.com>
2021-08-02 11:40:10 -0600

    convert to handlebars working, reg expression , fixed

--- server.js
+++ server.js
@@ -3,9 +3,12 @@ const express = require('express');
 const apiRoutes = require('./controllers/api');
 const sequelize = require('./db/connection');
 const session = require("express-session");
+const exphbs = require("express-handlebars");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+
 
 //start session
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -23,16 +26,26 @@ const sess = {
 
 app.use(session(sess));
 
-// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-// app.set('view engine', 'handlebars');
+// const helpers = require('./utils/helpers');
+
+// const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 
 // turn on routes
 app.use('/api', apiRoutes);
+
+app.get('/', (req,res) => {
+  res.render("index");
+});
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
